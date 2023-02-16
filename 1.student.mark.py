623d5d3689d31a26b395430f6a12428500140b04
Valentin Calmein <valcalmein@gmail.com>
2023-02-16 09:23:12 +0700

    Commit student's marks

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -7,8 +7,43 @@ while True:
         print("number of student unvailable")
 
 #student infromation 
+listnamestudent=[]
+listdobstudent=[]
+listidstudent=[]
 for i in range (nbstudent):
     print("student n°",i+1," : ")
     idstudent=input("what is the student's id : ")
+    listidstudent.append(idstudent)
     namestudent=input("what is the name of the student : ")
+    listnamestudent.append(namestudent)
     dobstudent=input("what is the DoB of the student : ")
+    listdobstudent.append(dobstudent)
+
+#number of courses
+while True:
+    try:
+        nbcourses=int(input("How many courses do they Have : "))
+        break
+    except:
+        print("number of student unvailable")
+
+#courses infromation
+listidcourse=[]
+listnamecourse=[]
+for i in range (nbcourses):
+    print("courses n°",i+1," : ")
+    idcourse=input("what is the coruse's id : ")
+    listidcourse.append(idcourse)
+    namecourse=input("what is the name of the course : ")
+    listnamecourse.append(namecourse)
+
+
+#marks student
+listmarks=[]
+for i in listnamecourse:
+    print("in",i,"...")
+    for x in listnamestudent:
+        print("what is the marks for",x," : ")
+        marks=input("")
+        listmarks.append(marks)
+        
